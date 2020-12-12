b3a9145c32df2cb1f8df23039ffd6661c31c9e43
Preston Lamb <preston.j.lamb@gmail.com>
2020-12-12 15:20:26 -0700

    test: 💍 create tests for plugin
    Create the test scripts for the plugin-canonical
    
    Closes: #1

--- plugin-canonical/src/plugin-canonical.spec.js
+++ plugin-canonical/src/plugin-canonical.spec.js
@@ -2,15 +2,43 @@ const { setCanonicalLink } = require('./plugin-canonical.js');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
-const STARTING_HTML = `<!DOCTYPE html> <html lang="en"> <head> <style>html { background: #dedede; }</style> <style>html { color: #444; }</style> <style>* { font-family: 'Arial'; }</style> <title>Test HTML File</title> </head> <body> </body> </html>`;
+const STARTING_HTML = `<!DOCTYPE html> <html lang="en"> <head> <link rel="canonical" href="https://example.com" /> <title>Test HTML File</title> </head> <body> </body> </html>`;
 
-test('it should convert multiple style tags into one style tag', async (done) => {
+test('it should replace the value of the href for the canonical link if the route object has a value', async (done) => {
+	const newHtml = await setCanonicalLink(STARTING_HTML, {
+		route: 'test-route',
+		canonical: 'https://example-changed.com',
+	});
+	const dom = new JSDOM(newHtml);
+	const { window } = dom;
+	const { document } = window;
+	const linkTag = document.querySelector('link[rel="canonical"]');
+
+	expect(linkTag.getAttribute('href')).toBe('https://example-changed.com');
+	done();
+});
+
+test('it should replace the value of the href for the canonical link if the route.data object has a value', async (done) => {
+	const newHtml = await setCanonicalLink(STARTING_HTML, {
+		route: 'test-route',
+		data: { canonical_url: 'https://example-changed.com' },
+	});
+	const dom = new JSDOM(newHtml);
+	const { window } = dom;
+	const { document } = window;
+	const linkTag = document.querySelector('link[rel="canonical"]');
+
+	expect(linkTag.getAttribute('href')).toBe('https://example-changed.com');
+	done();
+});
+
+test("it should not replace the value of the href for the canonical link if the route doesn't provide a replacement", async (done) => {
 	const newHtml = await setCanonicalLink(STARTING_HTML, { route: 'test-route' });
 	const dom = new JSDOM(newHtml);
 	const { window } = dom;
 	const { document } = window;
-	const styleTags = document.querySelectorAll('style');
+	const linkTag = document.querySelector('link[rel="canonical"]');
 
-	expect(styleTags.length).toBe(1);
+	expect(linkTag.getAttribute('href')).toBe('https://example.com');
 	done();
 });
